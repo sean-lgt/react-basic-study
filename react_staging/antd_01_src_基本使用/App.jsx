3cf9e546e408fd081eeb1259b54407d023fbb89a
sean-lgt <852951042@qq.com>
2022-02-26 14:12:25 +0800

    📦 antd 高级配置

--- react_staging/antd_01_src_基本使用/App.jsx
+++ react_staging/antd_01_src_基本使用/App.jsx
@@ -1,8 +1,6 @@
 import React, { Component } from 'react'
 import { Button,DatePicker } from 'antd';
-// 按需引入antd样式，不再全局引入
-// import "antd/dist/antd.css"
-
+import "antd/dist/antd.css"
 // 引入图标库
 import { WechatOutlined,SearchOutlined } from '@ant-design/icons';
 export default class App extends Component {
---
+++ react_staging/antd_02_src_高级配置/index.js
@@ -0,0 +1,13 @@
+// 引入React
+import React from "react"
+// 引入reactDom
+import ReactDOM from "react-dom"
+
+import {BrowserRouter} from "react-router-dom"
+
+// 引入App组件
+import App from "./App"
+
+ReactDOM.render(
+<BrowserRouter><App /></BrowserRouter>
+,document.getElementById("root"))
